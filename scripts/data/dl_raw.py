3d209aeb219511f025fad6959cd9c7f24408eafb
jejjohnson <jemanjohnson34@gmail.com>
2022-03-22 11:17:26 +0100

    added a data loading script.

---
+++ scripts/data/dl_raw.py
@@ -0,0 +1,20 @@
+from json.tool import main
+
+
+
+
+def main():
+    # load credentials files
+
+    # download train datasets
+
+    # download reference dataset
+
+    # download test dataset
+
+    
+    pass
+            
+
+if __name__ == '__main__':
+    main()
